4cafc06a8ea956a9de0601eb64a90299f64d3bcb
Danil Prokopenko <haosprinz@gmail.com>
2024-06-23 18:40:53 +0500

    Создал компонент карточка товара

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {productsMock} from '../../../shared/products/products.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    readonly product = productsMock[0];
+}
