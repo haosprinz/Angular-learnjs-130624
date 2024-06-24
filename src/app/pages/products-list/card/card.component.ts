3a50093756f6a4d00448b3a038dcd3dfff1013ee
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-06-24 20:03:07 +0300

    feat: add product card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {productsMock} from '../../../shared/products/products.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    readonly product = productsMock[0];
+
+    onProductBuy(event: Event) {
+        event.stopPropagation();
+
+        // eslint-disable-next-line no-console
+        console.log('Buy product');
+    }
+
+    isStarActive(starIndex: number): boolean {
+        return this.product.rating >= starIndex;
+    }
+}
