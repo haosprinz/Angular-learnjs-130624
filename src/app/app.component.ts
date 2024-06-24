d82e61812b485bcfa81f085406ef8c263e23f121
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-06-24 23:08:02 +0300

    feat: lecture 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit, AfterViewChecked {
+    counter = 0;
+
+    // constructor() {
+    //     setInterval(() => {
+    //         this.counter += 1;
+    //     }, 1000);
+    // }
+
+    ngAfterViewInit(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngAfterViewInit AppComponent');
+    }
+
+    ngAfterViewChecked(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngAfterViewChecked AppComponent');
+    }
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,57 @@
-import {Component} from '@angular/core';
+import {
+    AfterViewChecked,
+    AfterViewInit,
+    Component,
+    DoCheck,
+    OnDestroy,
+    OnInit,
+} from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent
+    implements OnInit, DoCheck, AfterViewInit, AfterViewChecked, OnDestroy
+{
     readonly title = 'Angular-learnjs-130624';
     readonly logoPath = '../../../favicon.ico';
+
+    constructor() {
+        // eslint-disable-next-line no-console
+        console.log('constructor HeaderComponent');
+    }
+
+    getTitle(): string {
+        // eslint-disable-next-line no-console
+        console.log('Recalculate view HeaderComponent');
+
+        return this.title;
+    }
+
+    ngOnInit(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngOnInit HeaderComponent');
+    }
+
+    ngDoCheck(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngDoCheck HeaderComponent');
+    }
+
+    ngAfterViewInit(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngAfterViewInit HeaderComponent');
+    }
+
+    ngAfterViewChecked(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngAfterViewChecked HeaderComponent');
+    }
+
+    ngOnDestroy(): void {
+        // eslint-disable-next-line no-console
+        console.log('ngOnDestroy HeaderComponent');
+    }
 }
